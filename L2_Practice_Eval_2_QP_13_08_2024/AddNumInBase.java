38a892c5bf35499ebd29677460b180da0e269c7a
Anu M <131898026+Anuledger2003@users.noreply.github.com>
2024-08-13 16:31:24 +0530

    Add files via upload

---
+++ L2_Practice_Eval_2_QP_13_08_2024/AddNumInBase.java
@@ -0,0 +1,17 @@
+package l2practiceeval2;
+
+/*
+		2. Add two numbers in the given base without converting into base.
+		Sample Input-1
+		123 13 4
+		Sample Output-1
+		202
+		Sample Input-2
+		1010 11001 2
+		Sample Output-2
+		100011
+ */
+
+public class AddNumInBase {
+
+}
